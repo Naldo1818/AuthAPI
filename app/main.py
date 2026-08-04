150afb07489774e1c25ffca9fe560b4bb42d27d8
Ronaldo Jansen <jansen.ronaldocullen@gmail.com>
2026-08-05 00:46:38 +0200

    protected

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from app.auth import router as auth_router
 from app.public import router as public_router
+from app.protected import router as protected_router
 
 app = FastAPI(
     title="Authentication API",
@@ -10,7 +11,7 @@ app = FastAPI(
 
 # Register the public routes
 app.include_router(public_router)
-
+app.include_router(protected_router)
 # Register the authentication routes
 app.include_router(auth_router)
 
--- app/protected.py
+++ app/protected.py
@@ -1,10 +1,10 @@
 from fastapi import APIRouter, Header, HTTPException, status
+from app.config import supabase
 
 router = APIRouter(
     prefix="/protected",
     tags=["Protected"]
 )
-
 
 @router.get("/profile")
 async def profile(authorization: str = Header(default=None)):
@@ -15,7 +15,27 @@ async def profile(authorization: str = Header(default=None)):
             detail="Access token required"
         )
 
-    return {
-        "message": "Authorization header received.",
-        "authorization": authorization
-    }
+    parts = authorization.split()
+
+    if len(parts) != 2 or parts[0] != "Bearer":
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid authorization header"
+        )
+
+    token = parts[1]
+
+    try:
+        response = supabase.auth.get_user(token)
+
+        return {
+            "id": response.user.id,
+            "email": response.user.email,
+            "created_at": response.user.created_at
+        }
+
+    except Exception:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid or expired token"
+        )
